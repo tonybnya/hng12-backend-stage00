2f58030a90bf27226ae756d469a746580eb9f202
tonybnya <nya.tony2010@gmail.com>
2025-01-29 15:05:00 +0100

    fix: put infos dict inside the route handler to generate a fresh timestamp every time the endpoint is called

--- backend/stage_0/main.py
+++ backend/stage_0/main.py
@@ -7,16 +7,15 @@ app = Flask(__name__)
 # enable CORS for all routes
 CORS(app)
 
-infos = {
-    "email": "tonybnya@gmail.com",
-    "current_datetime": get_current_iso_datetime(),
-    "github_url": "https://github.com/tonybnya/hng12/backend/stage_0",
-}
-
 
 # define the root endpoint
 @app.route("/")
 def home():
+    infos = {
+        "email": "tonybnya@gmail.com",
+        "current_datetime": get_current_iso_datetime(),
+        "github_url": "https://github.com/tonybnya/hng12/backend/stage_0",
+    }
     return jsonify(infos), 200
 
 
