68e056d0f023a48c4dede255f764c0fca54e9f12
tonybnya <nya.tony2010@gmail.com>
2025-01-30 11:07:32 +0100

    fix: change back the port to 5000

--- main.py
+++ main.py
@@ -22,5 +22,5 @@ def home():
 
 # run the Flask application
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5003))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
