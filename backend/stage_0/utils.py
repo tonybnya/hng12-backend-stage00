920821c7c53177193131186ba35cbc514efa39df
tonybnya <nya.tony2010@gmail.com>
2025-01-30 08:28:56 +0100

    fix: use Z instead of +00:00 to explicitly denote UTC format for datetime change the GitHub URL to the api folder

--- backend/stage_0/utils.py
+++ backend/stage_0/utils.py
@@ -2,4 +2,10 @@ from datetime import datetime, timezone
 
 
 def get_current_iso_datetime():
-    return datetime.now(timezone.utc).isoformat()
+    # return datetime.now(timezone.utc).isoformat()
+    return (
+        datetime.now(timezone.utc)
+        .replace(microsecond=0)
+        .isoformat()
+        .replace("+00:00", "Z")
+    )
